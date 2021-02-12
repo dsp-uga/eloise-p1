45a774e367113b5d856264982b9355e139853883
Kadir <bicekadir@gmail.com>
2021-02-12 00:34:42 -0500

    slight changes to prediction and accuracy

--- dsp_project1_kadir.py
+++ dsp_project1_kadir.py
@@ -6,9 +6,9 @@ Created on Thu Jan 28 14:37:57 2021
 @author: kadir
 """
 from pyspark import SparkContext, SparkConf
-import json, sys, os, math
+import os, math
 from google.cloud import storage
-import numpy as np
+# import numpy as np
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/kadir/Documents/Spyder/DSP_Spring21/google_cloud_key.json'
 # If you don't specify credentials when constructing the client, the
@@ -55,7 +55,6 @@ sc = SparkContext(conf = conf)
 
 # Gather document names (x) and related classes (y) (LOCAL NOW FOR PRACTICE) 
 # May need to broadcast this
-
 def join_x_and_y(x_train_file, y_train_file):
     x_small_train = sc.textFile(x_train_file).zipWithIndex().map(lambda x: (x[1], x[0]))
     y_small_train = sc.textFile(y_train_file).zipWithIndex().map(lambda x: (x[1], x[0]))
@@ -65,7 +64,6 @@ def join_x_and_y(x_train_file, y_train_file):
 # label-doc(y-x) pairs
 doc_labels = join_x_and_y('/home/kadir/Documents/Spyder/DSP_Spring21/project1/X_small_train.txt', 
               '/home/kadir/Documents/Spyder/DSP_Spring21/project1/y_small_train.txt' )#.filter(lambda x: x[0] == str(1)).collect()
-# np.array(doc_labels).reshape(-1, 2)[:,1][1]
 
 # filter x based on y (might be better with RDDs)
 small_train_dir = "/home/kadir/Documents/Spyder/DSP_Spring21/project1/small_train/"
@@ -76,16 +74,6 @@ def filter_file_names(class_index):
            addresses = addresses + small_train_dir + y_filtered_x[i] +'.bytes,'
     addresses = addresses[:-1]
     return addresses 
-
-# RDD containing class 1 documents
-class1_docs = sc.textFile(filter_file_names(1))
-
-# # class filtered documents RDD
-# class1 = sc.textFile("/home/kadir/Documents/Spyder/DSP_Spring21/project1/testing/2F6ZfVCQRi3vrwcj4zxL.bytes,/home/kadir/Documents/Spyder/DSP_Spring21/project1/testing/JnHGRI2v5NuB9lpsEOCS.bytes")
-# class1.count()
-
-# class3 = sc.textFile("/home/kadir/Documents/Spyder/DSP_Spring21/project1/testing/5QpgRV2cqU9wvjBist1a.bytes")
-# class3.count()
 
 ################ TRAINING ######################
 ################################################
@@ -113,17 +101,15 @@ def term_freq(document_set, vocabulary):
     processed = document_set.map(lambda x: x[9:]).flatMap(lambda x: x.strip().split()).map(lambda x: (x, 1)).union(vocabulary).reduceByKey(lambda x, y: x + y)
     return processed
 
-# term_freq(class1_docs, vocabulary_of_zeros(all_train, all_test)).sortBy(lambda x: x[1]).take(10)
-
 comp_vocabulary = vocabulary_of_zeros(all_train, all_test)
 
 no_unique_words = comp_vocabulary.count()
     
-### ALTERNATING CONDITIONAL PROBABILITY CALCULATION: ADDED NO OF UNIQUE WORDS(WHOLE VOCABULARY) TO DENOMINATOR
-### HOWEVER, NO OF WORD X IN ALL DOCUMENTS STAYS THE SAME (PER PROJECT MANUAL)
-### MIGHT NEED TO CHANGE THAT TO NO OF ALL WORDS IN DOC Y AS IN GENERAL APPLICATIONS
-
 # calculate cond probabilities P(x_i, y_k) including laplace smoothing and log transformation
+# ALTERNATING CONDITIONAL PROBABILITY CALCULATION: ADDED NO OF UNIQUE WORDS(WHOLE VOCABULARY) TO DENOMINATOR
+# HOWEVER, NO OF WORD X IN ALL DOCUMENTS STAYS THE SAME (PER PROJECT MANUAL)
+# MIGHT NEED TO CHANGE THAT TO NO OF ALL WORDS IN DOC Y AS IN GENERAL APPLICATIONS
+
 # def cond_prob(class_filtered, unfiltered_train):
 #     probs = term_freq(class_filtered, comp_vocabulary).join(term_freq(unfiltered_train, comp_vocabulary)).map(lambda x: (x[0], math.log10( (x[1][0] + 1)/(x[1][1] + no_unique_words) )))
 #     return probs
@@ -136,9 +122,7 @@ def cond_prob(class_filtered, unfiltered_train):
     probs = tf_class.map(lambda x: (x[0], math.log10( (x[1] + 1)/(total_words_in_class + no_unique_words) )))
     return probs
 
-# tst = cond_prob(class1_docs, all_train)
-
-# loop to get probability information for all classes (and potentially broadcast)
+# loop to get probability information for all classes and pass to prediction (and potentially broadcast)
 # get prior and conditional
 prior_prob_list = []
 cond_prob_list = []
@@ -147,40 +131,41 @@ for i in range(1,10):
     cond_prob_list.append(sc.broadcast(cond_prob(sc.textFile(filter_file_names(i)),all_train).collectAsMap()).value)
     # print(cond_prob_list[i-1].take(5))
 
-# prior_prob(doc_labels, 1)
-# bla = term_freq(class1_docs, comp_vocabulary)
-# bla.values().sum()
-# cond_probs_1 = cond_prob(class1_docs,all_train) #.sortBy(lambda x: -x[1])
-# cond_probs_1.take(5)
-
 ################ PREDICTION ######################
 ##################################################
 
 # get documents to be classified
 x_test_list = sc.broadcast(sc.textFile('/home/kadir/Documents/Spyder/DSP_Spring21/project1/X_small_test.txt').collect()).value
 y_test_list = sc.broadcast(sc.textFile('/home/kadir/Documents/Spyder/DSP_Spring21/project1/y_small_test.txt').collect()).value
-doc_classification = sc.parallelize([])
-for doc_i in range(len(x_test_list)):
-    # class_scores = np.zeros((9,2))
-    class_scores = []
-    for class_i in range (1,10):
-        filename = x_test_list[doc_i]
-        document1 = sc.textFile(("/home/kadir/Documents/Spyder/DSP_Spring21/project1/small_test/" + filename + ".bytes")).map(lambda x: x[9:]).flatMap(lambda x: x.strip().split()).map(lambda x: (x, cond_prob_list[class_i-1][x]))
-        class_scores.append([filename, class_i ,document1.values().sum() + prior_prob_list[class_i-1], y_test_list[doc_i]])
-        # class_scores[class_i-1,:] = [class_i ,document1.values().sum() + prior_prob_list[class_i-1]]
-    doc_classification = doc_classification.union(sc.parallelize([k for k in class_scores if k[2] == max(l[2] for l in class_scores)]))
 
-doc_classification.take(10)
+# predict classes (I attached labels(y) to the RDD as well for test purposes but we do not need it for later)
+def predict_classes(list_of_files):
+    doc_classification = sc.parallelize([])
+    for doc_i in range(10):
+        # class_scores = np.zeros((9,2))
+        class_scores = []
+        for class_i in range (1,10):
+            filename = list_of_files[doc_i]
+            document1 = sc.textFile(("/home/kadir/Documents/Spyder/DSP_Spring21/project1/small_test/" + filename + ".bytes")).map(lambda x: x[9:]).flatMap(lambda x: x.strip().split()).map(lambda x: (x, cond_prob_list[class_i-1][x]))
+            class_scores.append([filename, class_i ,document1.values().sum() + prior_prob_list[class_i-1], y_test_list[doc_i]])
+            # class_scores[class_i-1,:] = [class_i ,document1.values().sum() + prior_prob_list[class_i-1]]
+        doc_classification = doc_classification.union(sc.parallelize([k for k in class_scores if k[2] == max(l[2] for l in class_scores)]))
+    return doc_classification
 
-# class_scores[class_scores[:,1] == max(class_scores[:,1]),:]
+doc_classification = predict_classes(x_test_list)
 
+# doc_classification.take(10)
+
+# save as txt
+with open('classification.txt', 'w') as txt_file:
+  txt_file.writelines("%s\n" % p for p in doc_classification.map(lambda x: x[1]).collect())
+  
 ################ ACCURACY CHECK###################
 ##################################################
-
-doc_classification.map(lambda x: x[1] == int(x[3])).sum()/doc_classification.count()
-
-
-
+def accuracy_check(classifications):    
+    print('Accuracy of the classification: ' + str(format(100*classifications.map(lambda x: x[1] == int(x[3])).sum()/classifications.count(), '.2f')) + '%')
+    
+accuracy_check(doc_classification)
 
 
 
